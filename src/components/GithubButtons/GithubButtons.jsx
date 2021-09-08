95cc59c386d19baa96e2c466eb756e67a1c9fcc2
Kiran Naik <23kirannaik@gmail.com>
2021-09-08 13:46:47 +0530

    text

--- src/components/GithubButtons/GithubButtons.jsx
+++ src/components/GithubButtons/GithubButtons.jsx
@@ -5,21 +5,21 @@ const GithubButton = () => (
   <>
     <GitHubButton
       className="github-button"
-      href="https://github.com/cobidev/gatsby-simplefolio/fork"
+      href="https://github.com/kirannaikI/react-portfolio/fork"
       data-icon="octicon-repo-forked"
       data-size="large"
       data-show-count="true"
-      aria-label="Fork cobidev/gatsby-simplefolio on GitHub"
+      aria-label="Fork kirannaikI/react-portfolio on GitHub"
     >
       Fork
     </GitHubButton>
     <GitHubButton
       className="github-button"
-      href="https://github.com/cobidev/gatsby-simplefolio"
+      href="https://github.com/kirannaikI/react-portfolio"
       data-icon="octicon-star"
       data-size="large"
       data-show-count="true"
-      aria-label="Star cobidev/gatsby-simplefolio on GitHub"
+      aria-label="Star kirannaikI/react-portfolio on GitHub"
     >
       Star
     </GitHubButton>
